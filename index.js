60b40f8bc0f28447c14368ae25df0ae61edf9788
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-12 19:55:59 +0100

    Created function to initialize server and database

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const initializeDB = require("./database");
 require("dotenv").config();
+const { initializeServer } = require("./server");
 
-const initializeServer = () => {};
+const port = process.env.PORT ?? process.env.SERVER_PORT ?? 5000;
 
-module.exports = initializeServer;
+(async () => {
+  try {
+    await initializeServer(process.env.MONGODB_STRING);
+    initializeServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
