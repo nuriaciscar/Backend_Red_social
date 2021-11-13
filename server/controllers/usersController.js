6589d129eeb01ce2c1e86aafc8f541aabb8eaaa2
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-13 18:26:32 +0100

    Populate users with enemies and friends

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
-const debug = require("debug")("users:controller");
-const bcrypt = require("bcrypt");
-const chalk = require("chalk");
-const jwt = require("jsonwebtoken");
+
 const User = require("../../database/models/user");
 
 const getUsers = async (req, res, next) => {
-  const users = await User.find(req.body);
-  res.json(users);
+  try {
+    const users = await User.find({ id: user.id }).populate([
+      {
+        path: "friends",
+        select: "-password -username -friends -enemies ",
+      },
+      {
+        path: "enemies",
+        select: "-password -username -friends -enemies",
+      },
+    ]);
+    res.json(users);
+  } catch (error) {
+    error.message = "Cannot search the users";
+    error.code = 400;
+    next(error);
+  }
 };
 
 module.exports = { getUsers };
