a0829a9d4825329798bcd37064c7f5f78329db2f
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-14 19:31:27 +0100

    Added auth on usersRoutes

---
+++ server/middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
+    const error = new Error("Not authorized sorry");
+    error.code = 401;
+    next(error);
+  } else {
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      const error = new Error("Token is missing...");
+      error.code = 401;
+      next(error);
+    } else {
+      try {
+        const { id, username } = jwt.verify(token, process.env.SECRET);
+        req.userInfo = { id, username };
+        next();
+      } catch (error) {
+        error.message = "Token no valid";
+        error.code = 401;
+        next(error);
+      }
+    }
+  }
+};
+
+module.exports = auth;
